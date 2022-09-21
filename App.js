a7cad94de9ee0d39d8e86172b03ea07c457a3976
helmioo <81010386+helmioo@users.noreply.github.com>
2022-09-21 19:01:47 +0300

    tyylittelyä

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { StyleSheet, Text, ScrollView, Switch, TextInput, Button, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, Switch, TextInput, Button, View, Pressable } from 'react-native';
 import NumericInput from 'react-native-numeric-input';
-import RadioForm from 'react-native-simple-radio-button';
+import RadioForm, { RadioButtonInput } from 'react-native-simple-radio-button';
 import { DarkTheme, LightTheme } from './MyStyles';
 
 export default function App() {
@@ -60,18 +60,22 @@ export default function App() {
   return (
     <ScrollView style={theme.container}>
 
-      <Switch
-        onValueChange={(value) => setDark(value)}
-        value={dark}
-        thumbColor='#cecece'
-        trackColor={{ false: '#db9833', true: '#f8ead6' }}
-      />
-
-      <Text style={[LightTheme.boldText, LightTheme.title]}>Alcometer</Text>
+      <View style={LightTheme.start}>
+        <Switch
+          onValueChange={(value) => setDark(value)}
+          value={dark}
+          thumbColor='#cecece'
+          trackColor={{ false: '#db9833', true: '#f8ead6' }}
+        />
+      </View>
+      <View>
+        <Text style={[LightTheme.boldText, LightTheme.title]}>Alcometer</Text>
+      </View>
 
       <View style={LightTheme.middle}>
         <Text style={LightTheme.boldText}>Weight</Text>
         <TextInput
+          selectionColor={'black'}
           style={LightTheme.input}
           value={weight}
           onChangeText={text => setWeight(text)}
@@ -79,12 +83,14 @@ export default function App() {
         />
         <Text style={LightTheme.boldText}>Bottles</Text>
         <NumericInput
+          rounded
           style={LightTheme.input}
           value={bottles}
           onChange={value => setBottles(value)}
         />
         <Text style={LightTheme.boldText}>Hours</Text>
         <NumericInput
+          rounded
           style={LightTheme.input}
           value={hours}
           onChange={value => setHours(value)}
@@ -92,28 +98,23 @@ export default function App() {
         <RadioForm
           style={LightTheme.radioButton}
           labelStyle={LightTheme.boldText}
-          buttonColor={'#f8ead6'}
+          buttonColor={'black'}
+          selectedButtonColor={'#db9833'}
+          animation= {true}
           radio_props={genders}
           initial={0}
           onPress={value => setGender(value)}
-        />
+          />
       </View>
 
-      <Text>{result.toFixed(2)}</Text>
+      <Text style={LightTheme.end}>{result.toFixed(2)}</Text>
 
-      <Button style={LightTheme.button} title='CALCULATE' />
+      <Pressable style={LightTheme.end}>
+        <Text style={LightTheme.button}>Calculate</Text>
+      </Pressable>
 
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 // onPress={calculate}
--- MyStyles.js
+++ MyStyles.js
@@ -1,55 +1,82 @@
 import { StyleSheet } from "react-native"
 
 export const LightTheme = StyleSheet.create({
-   
-    container: {
-      paddingTop: 40,
-      flex: 1,
-      backgroundColor: '#db9833',
-      padding: 8,
-    },
-    title: {
-      fontWeight: 'bold',
-      fontSize: 28,
-      flexDirection: "row",
-      justifyContent: 'center',
-      alignContent: 'center',
-      textAlign: 'center',
-    },
-    boldText: {
-      fontWeight: 'bold',
-      fontSize: 20,
-      padding: 10,
-    },
-    input: {
-      backgroundColor: '#f8ead6',
-      marginEnd: 200,
-      height: 30,
-      borderRadius: 2,
-    },
-    radioButton: {
-      padding: 10,
-    },
-    middle: {
-      flex: 4,
-    
-    },
-    button:{
-      color: '#f8ead6',
-      margin: 30,
+
+  container: {
+    paddingTop: 60,
+    flex: 1,
+    backgroundColor: '#db9833',
+    padding: 8,
+  },
+  start: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    paddingStart: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    textDecorationLine: 'underline',
+    flexDirection: "row",
+    justifyContent: 'center',
+    alignContent: 'center',
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    padding: 10,
+  },
+  input: {
+    backgroundColor: '#db9833',
+    marginEnd: 200,
+    height: 30,
+    borderRadius: 2,
+
+  },
+  radioButton: {
+    padding: 10,
+  },
+  middle: {
+    flex: 4,
+
+  },
+  end: {
+    flexDirection: "row",
+    justifyContent: 'center',
+    alignContent: 'center',
+    textAlign: 'center',
+    fontSize: 33,
+    fontWeight: 'bold',
+  },
+  button: {
+    color: '#f8ead6',
+    fontSize: 20,
+    borderStyle:'solid',
+    borderColor: 'black',
+    marginTop: 30,
     flexDirection: "row",
     padding: 10,
-    backgroundColor: "#73CED6",
+    backgroundColor: '#db9833',
     width: 150,
     borderRadius: 15,
     justifyContent: 'center',
-    alignItems: 'center'
-    }
+    alignItems: 'center',
+    textAlign: 'center',
+  }
 })
-  
+
 export const DarkTheme = StyleSheet.create({
-      container: { ...LightTheme.boldText, ...LightTheme.container, ...LightTheme.header, backgroundColor: '#f8ead6'},
-      label:{
-        backgroundColor: 'yellow'
-      }
+  container: {
+    ...LightTheme.boldText,
+    ...LightTheme.container,
+    ...LightTheme.header,
+    backgroundColor: '#f8ead6',
+  },
+  input: {
+    backgroundColor: '#f8ead6',
+  },
+
 })
